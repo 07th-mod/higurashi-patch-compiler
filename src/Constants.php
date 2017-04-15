0985dc2a9e0ca9eb0c58c840dec75bd6ea150d3e
Jáchym Toušek <enumag@gmail.com>
2017-04-15 18:11:59 +0200

    Add links

--- src/Constants.php
+++ src/Constants.php
@@ -12,9 +12,21 @@ class Constants
     ];
 
     const PATCHES = [
+        'onikakushi' => [
+            'graphics' => 'https://gitlab.com/07th-mod/onikakushi-graphics/repository/archive.zip?ref=master',
+            'patch' => 'https://github.com/07th-mod/onikakushi/archive/master.zip',
+        ],
+        'watanagashi' => [
+            'graphics' => 'https://gitlab.com/07th-mod/watanagashi-graphics/repository/archive.zip?ref=master',
+            'patch' => 'https://github.com/07th-mod/watanagashi/archive/master.zip',
+        ],
         'tatarigoroshi' => [
             'graphics' => 'https://gitlab.com/07th-mod/tatarigoroshi-graphics/repository/archive.zip?ref=master',
             'patch' => 'https://github.com/07th-mod/tatarigoroshi/archive/master.zip',
+        ],
+        'himatsubushi' => [
+            'graphics' => 'https://gitlab.com/07th-mod/himatsubushi-graphics/repository/archive.zip?ref=master',
+            'patch' => 'https://github.com/07th-mod/himatsubushi/archive/master.zip',
         ],
     ];
 }
