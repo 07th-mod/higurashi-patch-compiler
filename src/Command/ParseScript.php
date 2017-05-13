19269d1f50681ebf0712ba8aad8369bbd78df607
Jáchym Toušek <enumag@gmail.com>
2017-05-13 12:11:16 +0200

    Fix parse command

--- src/Command/ParseScript.php
+++ src/Command/ParseScript.php
@@ -7,6 +7,7 @@ namespace Higurashi\Command;
 use Dibi\Connection;
 use Higurashi\Service\ScriptParser;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
@@ -17,12 +18,16 @@ class ParseScript extends Command
     {
         $this
             ->setName('higurashi:parse')
-            ->setDescription('Parses the PS3 script.');
+            ->setDescription('Parses the PS3 script.')
+            ->addArgument('script', InputArgument::REQUIRED, 'Path to the script.');
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $parser = new ScriptParser(__DIR__ . '/../../data/HigurashiPS3-Script.txt');
+        /** @var string $script */
+        $script = $input->getArgument('script');
+
+        $parser = new ScriptParser($script);
 
         $config = Yaml::parse(file_get_contents(__DIR__ . '/../../config/local.yml'));
 
