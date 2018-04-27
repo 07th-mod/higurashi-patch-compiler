ccc0a1b7d173add757624250a469120d9e5c5ebb
Jáchym Toušek <enumag@gmail.com>
2018-04-27 09:12:59 +0200

    Hide false positive errors

--- src/Command/DLLUpdate.php
+++ src/Command/DLLUpdate.php
@@ -122,7 +122,8 @@ class DLLUpdate extends Command
 
         if (Strings::match($line, '~^\\s++PlayVoice\\(~')) {
             if ($this->playingVoice && ! Strings::match($lines->get(-1), '~^\\s++PlayVoice\\(~')) {
-                echo sprintf('Found voice wait error in %s:%d.', $filename, $lineNumber) . PHP_EOL;
+                // This seems to report more false positives than real errors.
+                // echo sprintf('Found voice wait error in %s:%d.', $filename, $lineNumber) . PHP_EOL;
             }
 
             $this->playingVoice = true;
