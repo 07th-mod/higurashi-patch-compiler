b379c43829f76203d50d962da08cf4b6b4c4c0bd
Jáchym Toušek <enumag@gmail.com>
2018-07-04 20:12:34 +0200

    Generate sprite pack compiler in full upgrade

--- src/Command/FullArcUpgrade.php
+++ src/Command/FullArcUpgrade.php
@@ -122,8 +122,18 @@ class FullArcUpgrade extends Command
             $output
         );
 
+        $this->runCommand(
+            'higurashi:sprite-pack',
+            [
+                'chapter' => $chapter,
+            ],
+            $output
+        );
+
         $filesystem->remove(sprintf('%s/unpack/%s_patch', TEMP_DIR, $chapter));
-        $filesystem->mirror(sprintf('%s/voicepack/%s/Update', TEMP_DIR, $chapter), sprintf('%s/console/%s', TEMP_DIR, $chapter));
+        $filesystem->mirror(sprintf('%s/lipsync/%s/Update', TEMP_DIR, $chapter), sprintf('%s/full-upgrade/%s', TEMP_DIR, $chapter));
+        $filesystem->copy(sprintf('%s/voicepack/%s-voices.sh', TEMP_DIR, $chapter), sprintf('%s/full-upgrade/%s-voices.sh', TEMP_DIR, $chapter));
+        $filesystem->copy(sprintf('%s/spritepack/%s-sprites.sh', TEMP_DIR, $chapter), sprintf('%s/full-upgrade/%s-sprites.sh', TEMP_DIR, $chapter));
 
         return 0;
     }
